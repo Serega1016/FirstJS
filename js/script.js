329b1fa6dda0ef7aa4aa620e9dba87ff68bb21a9
Serg <serega1016@ukr.net>
2022-11-22 22:11:56 +0100

    add условия

--- js/script.js
+++ js/script.js
@@ -163,22 +163,62 @@
 //     }
 // Проверить, чтобы все работало без ошибок в консоли
 
-const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-  b = prompt("На сколько оцените его?", ""),
-  c = prompt("Один из последних просмотренных фильмов?", ""),
-  d = prompt("На сколько оцените его?", "");
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//   b = prompt("На сколько оцените его?", ""),
+//   c = prompt("Один из последних просмотренных фильмов?", ""),
+//   d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+// Условия
+
+// if (4 == 9) {
+//   console.log("ok");
+// } else {
+//   console.log("error");
+// }
+
+// const num = 50;
+// // бинарный
+// if (num < 49) {
+//   console.log("error");
+// } else if (num > 100) {
+//   console.log("much");
+// } else {
+//   console.log("ok");
+// }
+
+// // тернарный
+// (num === 50) ? console.log("ok") : console.log("error");
+// // унарный
+// +
+
+const num = 50;
+
+switch (num) {
+  case 49:
+    console.log("not");
+    break;
+  case 100:
+    console.log("not");
+    break;
+  case 51:
+    console.log("yes");
+    break;
+  default:
+    console.log("not now");
+    break;
+}
